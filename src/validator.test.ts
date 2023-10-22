93ff882253efe1dafe960afcabb6caf5e53d85f8
stonefruit <jeromeng888@gmail.com>
2023-10-22 23:24:19 +0800

    working validator

--- src/validator.test.ts
+++ src/validator.test.ts
@@ -13,6 +13,25 @@ describe('validateCellValues', () => {
       [1, 2, 3, 4, 5, 6, 7, 8, 9],
       [1, 2, 3, 4, 5, 6, 7, 8, 9],
     ];
-    expect(validateCellValues(gridValues)).toBe(false);
+    const flattened = gridValues.flat();
+    expect(validateCellValues(flattened)).toBe(false);
+  });
+  it('should check for valid grids', () => {
+    const gridValues = [
+      [4, 3, 5, 2, 6, 9, 7, 8, 1],
+      [6, 8, 2, 5, 7, 1, 4, 9, 3],
+      [1, 9, 7, 8, 3, 4, 5, 6, 2],
+      [8, 2, 6, 1, 9, 5, 3, 4, 7],
+      [3, 7, 4, 6, 8, 2, 9, 1, 5],
+      [9, 5, 1, 7, 4, 3, 6, 2, 8],
+      [5, 1, 9, 3, 2, 6, 8, 7, 4],
+      [2, 4, 8, 9, 5, 7, 1, 3, 6],
+      [7, 6, 3, 4, 1, 8, 2, 5, 9],
+    ];
+    const flattened = gridValues.flat();
+    expect(validateCellValues(flattened)).toBe(true);
+  });
+  it('should not allow wrong sized grids', () => {
+    expect(validateCellValues([1, 2, 3])).toBe(false);
   });
 });
--- src/validator.ts
+++ src/validator.ts
@@ -1,7 +1,45 @@
 import { Cell, Grid } from './types';
 
-const validateCellValues = (gridValues: number[][]): boolean => {
-  return false;
+const validateGroups = (values: number[][]): boolean => {
+  for (let i = 0; i < values.length; i += 1) {
+    const usedValues: number[] = [];
+    for (let j = 0; j < values.length; j += 1) {
+      if (usedValues.includes(values[i][j])) {
+        return false;
+      }
+      usedValues.push(values[i][j]);
+    }
+  }
+  return true;
+};
+
+const validateCellValues = (flatGrid: number[]): boolean => {
+  if (flatGrid.length !== 81) {
+    return false;
+  }
+  const rows: number[][] = [[], [], [], [], [], [], [], [], []];
+  const columns: number[][] = [[], [], [], [], [], [], [], [], []];
+  const squares: number[][] = [[], [], [], [], [], [], [], [], []];
+
+  for (let i = 0; i < flatGrid.length; i += 1) {
+    const value = flatGrid[i];
+
+    // ROWS
+    const rowIndex = Math.floor(i / 9);
+    rows[rowIndex].push(value);
+
+    // COLUMNS
+    const colIndex = i % 9;
+    columns[colIndex].push(value);
+
+    // SQUARES
+    const sqrIndex = Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3);
+    squares[sqrIndex].push(value);
+  }
+
+  return (
+    validateGroups(rows) && validateGroups(columns) && validateGroups(squares)
+  );
 };
 
 export { validateCellValues };
